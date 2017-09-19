ddfbd7b816ab5314cdcb9e2be792cfb03f476576
Mohammed Sadiq <sadiqmmm@gmail.com>
2017-09-19 17:45:45 +0530

    Add options feature working

---
+++ js/options.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	$('#saveLimit').click(function(){
+		var limit = $('#limit').val();		
+
+		if(limit) {
+			chrome.storage.sync.set({'limit': limit}, function(){
+				close();
+			});
+		}	
+	});
+
+	$('#resetTotal').click(function(){
+		chrome.storage.sync.set({'total': 0});
+	});
+});

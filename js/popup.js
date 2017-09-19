e4776971f9e20978edacc07c333a132971c739a2
Mohammed Sadiq <sadiqmmm@gmail.com>
2017-09-19 17:58:17 +0530

    Working chrome basic notification

--- js/popup.js
+++ js/popup.js
@@ -8,7 +8,7 @@ $(function(){
 	$('#spendAmount').click(function(){
 
 		// chrome storage get
-		chrome.storage.sync.get('total', function(budget){
+		chrome.storage.sync.get(['total','limit'], function(budget){
 			var newTotal = 0;
 
 			if (budget.total) {
@@ -21,7 +21,17 @@ $(function(){
 				newTotal += parseInt(amount);
 			}
 			// chrome storage set
-			chrome.storage.sync.set({'total': newTotal});
+			chrome.storage.sync.set({'total': newTotal}, function(){
+				if (amount && newTotal >= budget.limit){
+					var notifOptions = {
+						type: 'basic',
+						iconUrl: 'img/icon48.png',
+						title: 'Limit Reached!',
+						message: "Looks Like you have Reached your limit!"
+					};
+					chrome.notifications.create('limitNotif', notifOptions);
+				}
+			});
 
 			$('#total').text(newTotal);
 			$('#amount').val(''); 
